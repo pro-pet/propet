22e9c5bf96d4d40cdca397f771cdaff21afa2d53
Charles Lee <2535849195@qq.com>
2026-07-29 11:58:22 +0800

    refactor: update TypeScript paths in package.json and improve Spinner component props typing

--- packages/ui/src/components/spinner.tsx
+++ packages/ui/src/components/spinner.tsx
@@ -2,7 +2,7 @@ import { Loading03Icon } from '@hugeicons/core-free-icons'
 import { HugeiconsIcon } from '@hugeicons/react'
 import { cn } from '@propet/ui/lib/utils'
 
-function Spinner({ className, ...props }: React.ComponentProps<'svg'>) {
+function Spinner({ className, ...props }: Omit<React.ComponentProps<typeof HugeiconsIcon>, 'icon'>) {
   return (
     <HugeiconsIcon icon={Loading03Icon} strokeWidth={2} role="status" aria-label="Loading" className={cn('size-4 animate-spin', className)} {...props} />
   )
