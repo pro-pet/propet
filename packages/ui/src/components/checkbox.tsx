f9d49b1092bd75495248f2dc3d41689dcb27424a
Charles Lee <2535849195@qq.com>
2026-09-25 18:32:40 +0800

    fix checkbox migration lint

--- packages/ui/src/components/checkbox.tsx
+++ packages/ui/src/components/checkbox.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { Tick02Icon } from '@hugeicons/core-free-icons'
-import { HugeiconsIcon } from '@hugeicons/react'
-
 import { Checkbox as CheckboxPrimitive } from '@base-ui/react/checkbox'
+import { Tick02Icon } from '@hugeicons/core-free-icons'
+
+import { HugeiconsIcon } from '@hugeicons/react'
 import { cn } from '@propet/ui/lib/utils'
 import * as React from 'react'
 
