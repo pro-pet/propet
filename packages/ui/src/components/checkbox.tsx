15b6c46225b22624b5c8f0f3a5ea4b6507a47d30
Charles Lee <2535849195@qq.com>
2026-09-25 18:30:01 +0800

    migrate checkbox to Base UI

--- packages/ui/src/components/checkbox.tsx
+++ packages/ui/src/components/checkbox.tsx
@@ -3,19 +3,19 @@
 import { Tick02Icon } from '@hugeicons/core-free-icons'
 import { HugeiconsIcon } from '@hugeicons/react'
 
+import { Checkbox as CheckboxPrimitive } from '@base-ui/react/checkbox'
 import { cn } from '@propet/ui/lib/utils'
-import { Checkbox as CheckboxPrimitive } from 'radix-ui'
 import * as React from 'react'
 
 function Checkbox({
   className,
   ...props
-}: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
+}: CheckboxPrimitive.Root.Props) {
   return (
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        'border-input dark:bg-input/30 data-checked:bg-primary data-checked:text-primary-foreground dark:data-checked:bg-primary data-checked:border-primary aria-invalid:aria-checked:border-primary aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 focus:border-primary peer relative flex size-4 shrink-0 items-center justify-center rounded-[6px] border transition-colors outline-none group-has-disabled/field:opacity-50 after:absolute after:-inset-x-3 after:-inset-y-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'border-input dark:bg-input/30 data-checked:bg-primary data-checked:text-primary-foreground dark:data-checked:bg-primary data-checked:border-primary aria-invalid:aria-checked:border-primary aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 focus:border-primary peer relative flex size-4 shrink-0 items-center justify-center rounded-[6px] border transition-colors outline-none group-has-disabled/field:opacity-50 after:absolute after:-inset-x-3 after:-inset-y-2 data-disabled:cursor-not-allowed data-disabled:opacity-50',
         className,
       )}
       {...props}
